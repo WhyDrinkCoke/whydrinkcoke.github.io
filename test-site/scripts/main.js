a720b71f7de0c9090bfc154c3a8c355319eb5107
WhyDrinkCoke <116650953+WhyDrinkCoke@users.noreply.github.com>
2024-01-23 09:56:44 +0800

    Add test site files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,36 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "image/porsche.png") {
+    myImage.setAttribute("src", "image/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "image/porsche.png");
+  }
+};
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
